deae5985b38e782c16cbe647bf59dd87b8047763
Timur Valeyev <116timur116@gmail.com>
2022-11-13 12:24:00 +0600

    create custom redux

--- src/App.js
+++ src/App.js
@@ -13,37 +13,22 @@ import {Routes, Route} from "react-router-dom";
 import './App.scss';
 
 
-function App() {
-
-  const dialogData = [
-    {'id': 1, 'name': 'Jack', 'shortText': 'Hello, watsuuup...' },
-    {'id': 2, 'name': 'Mike', 'shortText': 'Call me back please...' },
-    {'id': 3, 'name': 'Sara', 'shortText': 'how are youdsdsdsdsdsdsdsdsdsdsdssdsdsd?...' },
-    {'id': 4, 'name': 'Dave', 'shortText': 'could you check email please...' },
-]
-
-const dialogInfo = [
-  {'id': 1, 'name': 'Jack', 'text': 'Hello, watsuuup...d asd asfsafas f fasf asf asf asf asf asf asf asf asf asf asf asf asf a' },
-  {'id': 2, 'name': 'Mike', 'text': 'Call me back please...sdfsdaggggggggggggggggggggggggggggggggggggggggggggggggggg' },
-  {'id': 3, 'name': 'Sara', 'text': 'how are youdsdsdsdsdsdsdsdsdsdsdssdsdsd?...' },
-  {'id': 4, 'name': 'Dave', 'text': 'could you check email please...asdasdddddddddddddddddddddddddddddd' },
-]
-
+function App({state}) {
 
   return (
     <div className="App">
         <Header />
         <main>
-            <SideBar dialogData={dialogData}/>
+            <SideBar dialogData={state.dialogData}/>
             <Routes>
                 <Route exact path='/' element={<Content />}/>
                 <Route exact path='/profile' element={<Profile />}/>
-                <Route path={`/dialogs/:name/:id`} element={<Dialog dialogInfo={dialogInfo} />}/>
+                <Route path={`/dialogs/:name/:id`} element={<Dialog dialogInfo={state.dialogInfo} />}/>
             </Routes>
         </main>
         <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/redux/state.js
@@ -0,0 +1,17 @@
+const state = {
+    dialogData: [
+        {'id': 1, 'name': 'Jack', 'shortText': 'Hello, watsuuup...' },
+        {'id': 2, 'name': 'Mike', 'shortText': 'Call me back please...' },
+        {'id': 3, 'name': 'Sara', 'shortText': 'how are youdsdsdsdsdsdsdsdsdsdsdssdsdsd?...' },
+        {'id': 4, 'name': 'Dave', 'shortText': 'could you check email please...' },
+    ],
+
+     dialogInfo: [
+        {'id': 1, 'name': 'Jack', 'text': 'Hello, watsuuup...d asd asfsafas f fasf asf asf asf asf asf asf asf asf asf asf asf asf a' },
+        {'id': 2, 'name': 'Mike', 'text': 'Call me back please...sdfsdaggggggggggggggggggggggggggggggggggggggggggggggggggg' },
+        {'id': 3, 'name': 'Sara', 'text': 'how are youdsdsdsdsdsdsdsdsdsdsdssdsdsd?...' },
+        {'id': 4, 'name': 'Dave', 'text': 'could you check email please...asdasdddddddddddddddddddddddddddddd' },
+    ]
+}
+
+export default state
